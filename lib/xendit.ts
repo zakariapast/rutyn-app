bfe49dea5a5ef9f06dcfd20965ce87c4d86454b2
zakariapast <37060701+zakariapast@users.noreply.github.com>
2025-06-08 21:38:30 +0800

    Update xendit.ts

--- lib/xendit.ts
+++ lib/xendit.ts
@@ -1,1 +1,37 @@
+// lib/xendit.ts
+import axios from 'axios';
+
+const XENDIT_SECRET_KEY = process.env.XENDIT_API_KEY!;
+
+export const createInvoice = async ({
+  externalId,
+  payerEmail,
+  description,
+  amount,
+  successRedirectURL
+}: {
+  externalId: string;
+  payerEmail: string;
+  description: string;
+  amount: number;
+  successRedirectURL: string;
+}) => {
+  const res = await axios.post('https://api.xendit.co/v2/invoices', {
+    external_id: externalId,
+    payer_email: payerEmail,
+    description,
+    amount,
+    success_redirect_url: successRedirectURL
+  }, {
+    auth: {
+      username: XENDIT_SECRET_KEY,
+      password: ''
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  return res.data.invoice_url;
+};
 
