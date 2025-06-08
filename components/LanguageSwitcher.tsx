ccacd1c0b6aba453a3a8b1d02e19588435309735
zakariapast <37060701+zakariapast@users.noreply.github.com>
2025-06-08 21:18:54 +0800

    Update LanguageSwitcher.tsx

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -3,14 +3,15 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 export default function LanguageSwitcher() {
-  const router = useRouter();
-  const { locales, locale, pathname } = router;
+  const { locales, locale, pathname, asPath } = useRouter();
 
   return (
-    <div className="mb-4 text-sm">
+    <div className="mb-4 flex gap-4">
       {locales?.map((lng) => (
-        <Link key={lng} href={pathname} locale={lng} className={lng === locale ? 'font-bold underline' : ''}>
-          {lng.toUpperCase()}
+        <Link key={lng} href={asPath} locale={lng}>
+          <button className={`px-3 py-1 border rounded ${lng === locale ? 'bg-blue-600 text-white' : ''}`}>
+            {lng.toUpperCase()}
+          </button>
         </Link>
       ))}
     </div>
