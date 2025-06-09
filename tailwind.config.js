ae4216eb2b34fc77aa41f6fe6f189ac8a51d1490
zakariapast <37060701+zakariapast@users.noreply.github.com>
2025-06-09 14:02:39 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  safelist: [
+    'text-deepBlue',
+    'bg-deepBlue',
+    'text-teal',
+    'bg-teal'
   ],
   theme: {
     extend: {
